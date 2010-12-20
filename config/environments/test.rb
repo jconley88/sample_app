8d06cac81854646cb278a46206fc17a6eafdaeb1
jconley <conley.jj@gmail.com>
2010-12-19 19:32:32 -0500

    Completed chapter 5 excersizes.  Moved logo code to a helper and updated integration testing to use webrat

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -28,3 +28,4 @@ config.action_mailer.delivery_method = :test
 # config.active_record.schema_format = :sql
 
   config.gem 'rspec-rails', :version => '>= 1.3.3', :lib => false unless File.directory?(File.join(Rails.root, 'vendor/plugins/rspec-rails'))
+  config.gem 'webrat', :verstion => '~> 0.7.2'
